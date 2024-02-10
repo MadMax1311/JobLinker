4387f9d975974041f926221028ea5ffff65ed663
@MaD Max <shyam13112@gmail.com>
2024-02-10 23:06:36 +0530

    upadte

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import LoginPage from './Components/LoginSignup/LoginSignup';
+import Home from './Components/Home/Home'
+import LandingPage from './Components/LandingPage/LandingPage';
+// import Navbar from './Components/Navbar/Navbar'
+// import Job from './Components/Job/Job'
+// import About from './Components/About/About'
+// import Blog from './Components/Blog/Blog'
+// import Footer from './Components/Footer/Footer'
+import { BrowserRouter, Route, Routes} from "react-router-dom"
 
-function App() {
-  return (
+const App = () => {
+  return (    
     <>
-      <BrowserRouter>
-        <Routes>
-         <Route path='/' element={<LoginSignup />} />
-        </Routes>
-      </BrowserRouter>
+    {/* <LoginPage/> */}
+    {/* <Home/>
+    <Job/>
+    <Blog/>
+    <About/>
+  <Footer/>  */}
+
+    {/* Login & Signup Browser  */}
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='' element={<Navbar />}/> */}
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter> 
     </>
-  );
+  )
 }
 
 export default App;
