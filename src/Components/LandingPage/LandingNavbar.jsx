ea6253455cb52a3d75bfbceefc59b2405d0f8e8e
@MaD Max <shyam13112@gmail.com>
2024-02-10 18:18:38 +0530

    add LandingPage

---
+++ src/Components/LandingPage/LandingNavbar.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import './landingnavbar.css'
+import { Link } from 'react-scroll';
+// import { MdMarkEmailRead } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { IoReorderThreeOutline } from "react-icons/io5";
+import { Link as Boom } from 'react-router-dom';
+
+const LandingNavbar = () => {
+    // Code to show/Toggle navbar
+    const [active, setActive] = useState('navBar')
+    const showNav =()=>{
+        setActive('navBar activeNavbar')
+    }
+
+    // code to remove navbar
+    const removeNav =()=>{
+        setActive('navBar')
+    }
+
+    // function submit(e) {
+    //     e.preventDefault();
+    //     // const { id } = e;
+    //     // console.log(id)
+    //     document.querySelector(".homeCard").scrollIntoView();
+    //  }
+
+    // Code to add background color to the header
+    const [transparent, setTransparent] = useState('header')
+    const addBg = ()=>{
+        if(window.scrollY >= 10){
+            setTransparent('header activeHeader')
+        }
+        else{
+            setTransparent('header')
+        }
+    }
+    window.addEventListener('scroll', addBg)
+
+  return (
+    <section className='navBarSection'>
+        {/* <Link to='/login'></Link> */}
+        <div className={transparent}>
+            <div className='logoDiv'>
+                <a href='#' className='logo'>
+                    <h1 className='flex'>
+                        {/* <MdMarkEmailRead className="icon"/> */}
+                    Job
+                    <h1 className='fl'>Linker</h1>
+                    </h1>
+                </a>
+            </div>
+
+            <div className= {active}>
+            <ul className='navLists flex'>
+
+        {/* Here use the id for the scroll and in ever file it can help */}
+                <li className='navItem'>
+                    <Link  to="Home" spy={true} smooth={true} offset={-100} duration={2000} className='navLink'>Home</Link>
+                </li>
+
+                {/* <li className='navItem'>
+                    <Link  to="E-mail" spy={true} smooth={true} offset={-100} duration={500} className='navLink'>E-mail</Link>
+                </li> */}
+
+                {/* <li className='navItem'>
+                    <Link  to="Blog" spy={true} smooth={true} offset={-100} duration={500} className='navLink'>Blog</Link>
+                </li> */}
+
+                <li className='navItem'>
+                    <Link  to="About" spy={true} smooth={true} offset={-100} duration={500} className='navLink'>About</Link>
+                </li>
+
+                <li className='navItem'>
+                    <Link  to="Contacts" spy={true} smooth={true} offset={-120} duration={2000} className='navLink'>Contacts</Link>
+                </li>
+
+                <div className='headerBtns flex'>
+
+                    <div className='btn loginBtn'>
+                        {/* <a href='#'>Login</a> */}
+                        
+                        <Boom to='/login'>Login</Boom>
+                        
+                    </div>
+
+                    <button className='btn loginBtn'>
+                        {/* <Link to='/login'>Sign Up</Link> */}
+                        
+                        <Boom to='/login'>Sign  Up</Boom>
+                    </button>
+                </div>
+                </ul>
+
+                <div onClick = {removeNav} className='closeNavbar'>
+                    <AiFillCloseCircle className='icon' />
+                </div>
+            </div>
+            <div onClick={showNav} className='toggleNavbar'>
+                    <IoReorderThreeOutline className='ic'/>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default LandingNavbar
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,95 @@
+import React, {useState, useEffect} from "react";
+import "./landingpage.css";
+import img from "../../Assets/Home_Bg.png"
+import { FaUpload } from "react-icons/fa";
+import LandingNavbar from "./LandingNavbar";
+import Offers from '../Offers/Offers'
+// / / import Popular from './Components/Popular/Popular'
+import About from '../About/About'
+import Blog from '../Blog/Blog'
+import Footer from '../Footer/Footer'
+import { useNavigate } from "react-router-dom";
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+
+const LandingPage = () => {
+  const [fileUploaded, setFileUploaded] = useState(false);
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, [])
+  
+  
+  const uploadFiles = () => {
+    document.getElementById('selectFiles').click();
+  };
+
+
+  const navigateLogin = () => {
+    navigate('/login')
+  };
+  //  setTimeout(() => {
+  //     setFileUploaded(true);
+  //   }, 1000);
+  // };
+  
+  return (
+    <>
+    <LandingNavbar />
+    <section className="home" id="Home">
+          <div data-aos="fade-up" data-aos-duration="4000" className="img">
+            <img src={img} alt="" />
+          </div>
+      <div className="secContainer container">
+        <div className="homeText">
+          <h1 data-aos="fade-up"  className="title">Place your Job role & <br />Generate E-mail</h1>
+
+          <p data-aos="fade-up" data-aos-duration="4000" className="subTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            perferendis.
+          </p>
+
+          <button data-aos="fade-up" data-aos-duration="3000" className="btn">
+            <a href="#">Explore Now</a>
+          </button>
+        </div>
+
+        <div className="homeCard grid">
+          <div data-aos="fade-right" data-aos-duration="2000" className="locationDiv">
+            <label htmlFor="location">Job Role</label>
+            <input type="text" placeholder="Job Role" />
+          </div>
+
+          <div data-aos="fade-right" data-aos-duration="2500" className="distDiv">
+            <label htmlFor="distance">City</label>
+            <input type="text" placeholder="City"/>
+          </div>
+
+          <div data-aos="fade-right" data-aos-duration="3000" className="priceDiv">
+            <label For="uploadBtn">Upload Resume</label>
+            <button className="upload" onClick={uploadFiles}>
+            <FaUpload className="hi" />
+          </button>
+          <input type="file" id="selectFiles" name="upload" accept="application/pdf,application/" />
+          </div>
+          {/* {fileUploaded && (
+        <div style={{ marginTop: '10px', color: 'green' }}>
+          File uploaded successfully!
+        </div>
+      )} */}
+
+          <button data-aos="fade-up" data-aos-duration="3000" className="btn" onClick={navigateLogin} >Submit</button>
+        </div>
+      </div>
+    </section>
+    <Offers/>
+    <Blog/>
+    <About/>
+  <Footer/> 
+    </>
+  );
+};
+
+export default LandingPage;
